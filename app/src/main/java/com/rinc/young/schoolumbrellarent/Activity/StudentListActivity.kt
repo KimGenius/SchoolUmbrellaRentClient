64f7be4cdbbe812c0df851370e4f09ff38ceffe8
Kim0Jae <youngjae1047@naver.com>
2017-07-11 23:50:42 +0900

    Add Recycler code

--- app/src/main/java/com/rinc/young/schoolumbrellarent/Activity/StudentListActivity.kt
+++ app/src/main/java/com/rinc/young/schoolumbrellarent/Activity/StudentListActivity.kt
@@ -2,7 +2,10 @@ package com.rinc.young.schoolumbrellarent.Activity
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import com.rinc.young.schoolumbrellarent.Adapter.StudentListAdapter
 import com.rinc.young.schoolumbrellarent.R
+import kotlinx.android.synthetic.main.activity_student_list.*
 
 /**
  * Created by young on 2017-07-11.
@@ -12,5 +15,9 @@ class StudentListActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_list)
+
+        val listsLayoutManager = GridLayoutManager(applicationContext, 1)
+        lists.layoutManager = listsLayoutManager
+        val adapter = StudentListAdapter(applicationContext, "")
     }
 }
---
+++ app/src/main/java/com/rinc/young/schoolumbrellarent/Adapter/StudentListAdapter.kt
@@ -0,0 +1,29 @@
+package com.rinc.young.schoolumbrellarent.Adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+/**
+ * Created by young on 2017-07-11.
+ */
+class StudentListAdapter constructor(context: Context, json: String) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    init {
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
