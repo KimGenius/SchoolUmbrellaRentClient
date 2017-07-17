0fba12170f84ddcbec99f71cd9410e994a8922ea
Kim0Jae <youngjae1047@naver.com>
2017-07-17 15:36:30 +0900

    Edit routes

--- app/src/main/java/com/rinc/young/schoolumbrellarent/network/RetroAPIServer.kt
+++ app/src/main/java/com/rinc/young/schoolumbrellarent/network/RetroAPIServer.kt
@@ -4,39 +4,34 @@ import com.rinc.young.schoolumbrellarent.models.Student
 import com.rinc.young.schoolumbrellarent.util.StudentList
 import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.POST
+import retrofit2.http.*
 
 /**
-* Created by kimyoungjae on 2017. 2. 23./오후 3:09
-* This Project is SchoolUmbrellaRent
-*/
+ * Created by kimyoungjae on 2017. 2. 23./오후 3:09
+ * This Project is SchoolUmbrellaRent
+ */
 
 interface RetroAPIServer {
     @FormUrlEncoded
     @POST("/user/login")
     fun login(@Field("id") id: String, @Field("pw") pw: String): Call<ResponseBody>
 
-    @POST("/getStudentList")
-    fun getStudentList(): Call<StudentList>
-
-    @FormUrlEncoded
-    @POST("/getStudent")
-    fun getStudent(@Field("num") num: String): Call<Student>
-
     @FormUrlEncoded
     @POST("/addRent")
     fun addRent(@Field("idx") idx: String, @Field("date") date: String, @Field("umbrella") umbrella: String): Call<ResponseBody>
 
-    @FormUrlEncoded
-    @POST("/findStudents")
-    fun findStudents(@Field("name") name: String): Call<StudentList>
+    @GET("/getStudentList")
+    fun getStudentList(): Call<StudentList>
 
-    @FormUrlEncoded
-    @POST("/sortStudents")
-    fun sortStudents(@Field("type") type: String = "num", @Field("gradeSc") gradeSc: String, @Field("rentSc") rentSc: String): Call<StudentList>
+    @GET("/getStudent")
+    fun getStudent(@Query("num") num: String): Call<Student>
 
-    @POST("/getRentList")
+    @GET("/findStudents")
+    fun findStudents(@Query("name") name: String): Call<StudentList>
+
+    @GET("/sortStudents")
+    fun sortStudents(@Query("type") type: String = "num", @Query("gradeSc") gradeSc: String, @Query("rentSc") rentSc: String): Call<StudentList>
+
+    @GET("/getRentList")
     fun getRentList(): Call<StudentList>
 }
